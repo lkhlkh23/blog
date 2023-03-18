ae0f700d69a3fdb229336da4cc7464a31153dcce
lkhlkh23 <lkhlkh09@gmail.com>
2023-03-19 01:12:36 +0900

    create search api controller, dto, mapper

--- src/main/java/bank/blog/domain/search/SortType.java
+++ src/main/java/bank/blog/domain/search/SortType.java
@@ -9,11 +9,22 @@ import lombok.Getter;
 public enum SortType {
 
     ACCURACY("01", "accuracy", "sim", "정확도순"),
-    RECENCY("02", "recency", "date", "최신순");
+    RECENCY("02", "recency", "date", "최신순"),
+    NONE("", "none", "none", "디폴트 객체");
 
     private String code;
     private String kakoCode;
     private String naverCode;
     private String description;
+
+    public static SortType getSortTypeByCode(final String code) {
+        for (final SortType sortType : values()) {
+            if(code.equals(sortType.code)) {
+                return sortType;
+            }
+        }
+
+        return NONE;
+    }
 
 }
---
+++ src/test/java/bank/blog/controller/v1/GetBlogSearchControllerIntegrationTest.java
@@ -0,0 +1,74 @@
+package bank.blog.controller.v1;
+
+import bank.blog.controller.v1.dto.ResponseV1;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class GetBlogSearchControllerIntegrationTest {
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    @DisplayName("page가 1보다 작을 때, BadRequest 발생")
+    void test_search_whenPageIsLessThenOneThenReturnBadRequest() {
+        final ResponseEntity<ResponseV1> response =
+            this.restTemplate.exchange(String.format("/kakao/v1/search?query=%s&sort=%s&page=%d&size=%d", "kakao", "01", -1, 10)
+                , HttpMethod.GET, null, new ParameterizedTypeReference<ResponseV1>() {
+                });
+
+        assertEquals(HttpStatus.BAD_REQUEST, response.getStatusCode());
+        assertNotNull(response.getBody());
+        assertEquals(400, response.getBody().getStatus());
+    }
+
+    @Test
+    @DisplayName("size 1보다 작을 때, BadRequest 발생")
+    void test_search_whenSizeIsLessThenOneThenReturnBadRequest() {
+        final ResponseEntity<ResponseV1> response =
+            this.restTemplate.exchange(String.format("/kakao/v1/search?query=%s&sort=%s&page=%d&size=%d", "kakao", "01", 10, -1)
+                , HttpMethod.GET, null, new ParameterizedTypeReference<ResponseV1>() {
+                });
+
+        assertEquals(HttpStatus.BAD_REQUEST, response.getStatusCode());
+        assertNotNull(response.getBody());
+        assertEquals(400, response.getBody().getStatus());
+    }
+
+    @Test
+    @DisplayName("page가 50보다 클 때, BadRequest 발생")
+    void test_search_whenPageIsGreaterThen50ThenReturnBadRequest() {
+        final ResponseEntity<ResponseV1> response =
+            this.restTemplate.exchange(String.format("/kakao/v1/search?query=%s&sort=%s&page=%d&size=%d", "kakao", "01", 100, 10)
+                , HttpMethod.GET, null, new ParameterizedTypeReference<ResponseV1>() {
+                });
+
+        assertEquals(HttpStatus.BAD_REQUEST, response.getStatusCode());
+        assertNotNull(response.getBody());
+        assertEquals(400, response.getBody().getStatus());
+    }
+
+    @Test
+    @DisplayName("size 50보다 클 때, BadRequest 발생")
+    void test_search_whenSizeIsGreaterThen50ThenReturnBadRequest() {
+        final ResponseEntity<ResponseV1> response =
+            this.restTemplate.exchange(String.format("/kakao/v1/search?query=%s&sort=%s&page=%d&size=%d", "kakao", "01", 10, 100)
+                , HttpMethod.GET, null, new ParameterizedTypeReference<ResponseV1>() {
+                });
+
+        assertEquals(HttpStatus.BAD_REQUEST, response.getStatusCode());
+        assertNotNull(response.getBody());
+        assertEquals(400, response.getBody().getStatus());
+    }
+}
