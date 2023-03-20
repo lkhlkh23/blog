72587afeb227e4caaa72d9eccabf7478d5e55443
lkhlkh23 <lkhlkh09@gmail.com>
2023-03-20 11:03:34 +0900

    add test codes in GetSearchService

--- src/test/java/bank/blog/service/search/GetSearchServiceTest.java
+++ src/test/java/bank/blog/service/search/GetSearchServiceTest.java
@@ -5,6 +5,7 @@ import bank.blog.domain.search.SortType;
 import bank.blog.remote.kakao.KakaoSearchServiceImpl;
 import bank.blog.remote.naver.NaverSearchServiceImpl;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -14,6 +15,7 @@ import java.util.Collections;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.when;
 
 @SpringBootTest
@@ -41,6 +43,7 @@ class GetSearchServiceTest {
     }
 
     @Test
+    @DisplayName("카카오 검색결과가 있을 때, 카카오 결과 반환")
     void test_search_whenKakaoIsSuccessThenReturnKakaoResults() {
         // when
         when(kakaoSearchService.search(command)).thenReturn(List.of(createSearchDocument("kakao")));
@@ -53,6 +56,7 @@ class GetSearchServiceTest {
     }
 
     @Test
+    @DisplayName("카카오 검색결과가 없을 때, 네이버 결과 반환")
     void test_search_whenKakaoIsFailedThenReturnNaverResults() {
         // when
         when(kakaoSearchService.search(command)).thenReturn(Collections.EMPTY_LIST);
@@ -62,6 +66,17 @@ class GetSearchServiceTest {
         final List<SearchDocument> results = sut.search(command);
         assertEquals(1, results.size());
         assertEquals("naver", results.get(0).getTitle());
+    }
+
+    @Test
+    @DisplayName("카카오와 네이버 검색결과가 모두 없을 때, 빈 리스트 반환")
+    void test_search_whenAllFailedThenReturnEmptyListResults() {
+        // when
+        when(kakaoSearchService.search(command)).thenReturn(Collections.EMPTY_LIST);
+        when(naverSearchService.search(command)).thenReturn(Collections.EMPTY_LIST);
+
+        // then
+        assertTrue(sut.search(command).isEmpty());
     }
 
     private SearchDocument createSearchDocument(final String title) {
