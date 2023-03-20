a1af72c4c12efe69e0b663f5066862252720a9f8
lkhlkh23 <lkhlkh09@gmail.com>
2023-03-20 11:15:18 +0900

    apply search engine can be added

--- src/main/java/bank/blog/service/search/SearchServiceSelector.java
+++ src/main/java/bank/blog/service/search/SearchServiceSelector.java
@@ -2,22 +2,18 @@ package bank.blog.service.search;
 
 import bank.blog.remote.common.RemoteSearchService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Component
 public class SearchServiceSelector {
 
-    private final List<RemoteSearchService> selectors = new ArrayList<>();
+    private List<RemoteSearchService> selectors;
 
     @Autowired
-    public SearchServiceSelector(@Qualifier("kakaoSearchService") RemoteSearchService kakaoSearch,
-                                 @Qualifier("naverSearchService") RemoteSearchService naverSearch) {
-        selectors.add(kakaoSearch);
-        selectors.add(naverSearch);
+    public SearchServiceSelector(final List<RemoteSearchService> allSearchServices) {
+        this.selectors = allSearchServices;
     }
 
     public List<RemoteSearchService> getAllSearchServices() {
